876e29eaaaff5df1a60ac960015c2d61b0b0f1e7
Brandon Liu <bdon@bdon.org>
2022-09-28 11:34:50 +0800

    spec version 3 header serialize/deserialize functions

--- pmtiles/directory.go
+++ pmtiles/directory.go
@@ -5,7 +5,55 @@ import (
 	"bytes"
 	"compress/gzip"
 	"encoding/binary"
+	"math"
 )
+
+type Compression uint8
+
+const (
+	UnknownCompression Compression = 0
+	Gzip                           = 1
+	Brotli                         = 2
+	Zstd                           = 3
+)
+
+type TileType uint8
+
+const (
+	UnknownTileType TileType = 0
+	Mvt                      = 1
+	Png                      = 2
+	Jpeg                     = 3
+	Webp                     = 4
+)
+
+type HeaderV3 struct {
+	SpecVersion         uint8
+	RootOffset          uint64
+	RootLength          uint64
+	MetadataOffset      uint64
+	MetadataLength      uint64
+	LeafDirectoryOffset uint64
+	LeafDirectoryLength uint64
+	TileDataOffset      uint64
+	TileDataLength      uint64
+	AddressedTilesCount uint64
+	TileEntriesCount    uint64
+	TileContentsCount   uint64
+	Clustered           bool
+	InternalCompression Compression
+	TileCompression     Compression
+	TileType            TileType
+	MinZoom             uint8
+	MaxZoom             uint8
+	MinLon              float32
+	MinLat              float32
+	MaxLon              float32
+	MaxLat              float32
+	CenterZoom          uint8
+	CenterLon           float32
+	CenterLat           float32
+}
 
 type EntryV3 struct {
 	TileId    uint64
@@ -90,4 +138,68 @@ func deserialize_entries(data bytes.Buffer) []EntryV3 {
 	}
 
 	return entries
+}
+
+func serialize_header(header HeaderV3) []byte {
+	b := make([]byte, 122)
+	copy(b[0:2], "PM")
+
+	b[2] = 3
+	binary.LittleEndian.PutUint64(b[3:3+8], header.RootOffset)
+	binary.LittleEndian.PutUint64(b[11:11+8], header.RootLength)
+	binary.LittleEndian.PutUint64(b[19:19+8], header.MetadataOffset)
+	binary.LittleEndian.PutUint64(b[27:27+8], header.MetadataLength)
+	binary.LittleEndian.PutUint64(b[35:35+8], header.LeafDirectoryOffset)
+	binary.LittleEndian.PutUint64(b[43:43+8], header.LeafDirectoryLength)
+	binary.LittleEndian.PutUint64(b[51:51+8], header.TileDataOffset)
+	binary.LittleEndian.PutUint64(b[59:59+8], header.TileDataLength)
+	binary.LittleEndian.PutUint64(b[67:67+8], header.AddressedTilesCount)
+	binary.LittleEndian.PutUint64(b[75:75+8], header.TileEntriesCount)
+	binary.LittleEndian.PutUint64(b[83:83+8], header.TileContentsCount)
+	if header.Clustered {
+		b[91] = 0x1
+	}
+	b[92] = uint8(header.InternalCompression)
+	b[93] = uint8(header.TileCompression)
+	b[94] = uint8(header.TileType)
+	b[95] = header.MinZoom
+	b[96] = header.MaxZoom
+	binary.LittleEndian.PutUint32(b[97:97+4], math.Float32bits(header.MinLon))
+	binary.LittleEndian.PutUint32(b[101:101+4], math.Float32bits(header.MinLat))
+	binary.LittleEndian.PutUint32(b[105:105+4], math.Float32bits(header.MaxLon))
+	binary.LittleEndian.PutUint32(b[109:109+4], math.Float32bits(header.MaxLat))
+	b[113] = header.CenterZoom
+	binary.LittleEndian.PutUint32(b[114:114+4], math.Float32bits(header.CenterLon))
+	binary.LittleEndian.PutUint32(b[118:118+4], math.Float32bits(header.CenterLat))
+	return b
+}
+
+func deserialize_header(d []byte) HeaderV3 {
+	h := HeaderV3{}
+	h.RootOffset = binary.LittleEndian.Uint64(d[3 : 3+8])
+	h.RootLength = binary.LittleEndian.Uint64(d[11 : 11+8])
+	h.MetadataOffset = binary.LittleEndian.Uint64(d[19 : 19+8])
+	h.MetadataLength = binary.LittleEndian.Uint64(d[27 : 27+8])
+	h.LeafDirectoryOffset = binary.LittleEndian.Uint64(d[35 : 35+8])
+	h.LeafDirectoryLength = binary.LittleEndian.Uint64(d[43 : 43+8])
+	h.TileDataOffset = binary.LittleEndian.Uint64(d[51 : 51+8])
+	h.TileDataLength = binary.LittleEndian.Uint64(d[59 : 59+8])
+	h.AddressedTilesCount = binary.LittleEndian.Uint64(d[67 : 67+8])
+	h.TileEntriesCount = binary.LittleEndian.Uint64(d[75 : 75+8])
+	h.TileContentsCount = binary.LittleEndian.Uint64(d[83 : 83+8])
+	h.Clustered = (d[91] == 0x1)
+	h.InternalCompression = Compression(d[92])
+	h.TileCompression = Compression(d[93])
+	h.TileType = TileType(d[94])
+	h.MinZoom = d[95]
+	h.MaxZoom = d[96]
+	h.MinLon = math.Float32frombits(binary.LittleEndian.Uint32(d[97 : 97+4]))
+	h.MinLat = math.Float32frombits(binary.LittleEndian.Uint32(d[101 : 101+4]))
+	h.MaxLon = math.Float32frombits(binary.LittleEndian.Uint32(d[105 : 105+4]))
+	h.MaxLat = math.Float32frombits(binary.LittleEndian.Uint32(d[109 : 109+4]))
+	h.CenterZoom = d[113]
+	h.CenterLon = math.Float32frombits(binary.LittleEndian.Uint32(d[114 : 114+4]))
+	h.CenterLat = math.Float32frombits(binary.LittleEndian.Uint32(d[118 : 118+4]))
+
+	return h
 }
--- pmtiles/directory_test.go
+++ pmtiles/directory_test.go
@@ -52,3 +52,105 @@ func TestDirectoryRoundtrip(t *testing.T) {
 		t.Fatalf(`expected to be 2`)
 	}
 }
+
+func TestHeaderRoundtrip(t *testing.T) {
+	header := HeaderV3{}
+	header.RootOffset = 1
+	header.RootLength = 2
+	header.MetadataOffset = 3
+	header.MetadataLength = 4
+	header.LeafDirectoryOffset = 5
+	header.LeafDirectoryLength = 6
+	header.TileDataOffset = 7
+	header.TileDataLength = 8
+	header.AddressedTilesCount = 9
+	header.TileEntriesCount = 10
+	header.TileContentsCount = 11
+	header.Clustered = true
+	header.InternalCompression = Gzip
+	header.TileCompression = Brotli
+	header.TileType = Mvt
+	header.MinZoom = 1
+	header.MaxZoom = 2
+	header.MinLon = 1.1
+	header.MinLat = 2.1
+	header.MaxLon = 1.2
+	header.MaxLat = 2.2
+	header.CenterZoom = 3
+	header.CenterLon = 3.1
+	header.CenterLat = 3.2
+	b := serialize_header(header)
+	result := deserialize_header(b)
+	if result.RootOffset != 1 {
+		t.Fatalf(`expected to be 1`)
+	}
+	if result.RootLength != 2 {
+		t.Fatalf(`expected to be 2`)
+	}
+	if result.MetadataOffset != 3 {
+		t.Fatalf(`expected to be 3`)
+	}
+	if result.MetadataLength != 4 {
+		t.Fatalf(`expected to be 4`)
+	}
+	if result.LeafDirectoryOffset != 5 {
+		t.Fatalf(`expected to be 5`)
+	}
+	if result.LeafDirectoryLength != 6 {
+		t.Fatalf(`expected to be 6`)
+	}
+	if result.TileDataOffset != 7 {
+		t.Fatalf(`expected to be 7`)
+	}
+	if result.TileDataLength != 8 {
+		t.Fatalf(`expected to be 8`)
+	}
+	if result.AddressedTilesCount != 9 {
+		t.Fatalf(`expected to be 9`)
+	}
+	if result.TileEntriesCount != 10 {
+		t.Fatalf(`expected to be 10`)
+	}
+	if result.TileContentsCount != 11 {
+		t.Fatalf(`expected to be 11`)
+	}
+	if !result.Clustered {
+		t.Fatalf(`expected to be true`)
+	}
+	if result.InternalCompression != Gzip {
+		t.Fatalf(`expected to be gzip`)
+	}
+	if result.TileCompression != Brotli {
+		t.Fatalf(`expected to be brotli`)
+	}
+	if result.TileType != Mvt {
+		t.Fatalf(`expected to be mvt`)
+	}
+	if result.MinZoom != 1 {
+		t.Fatalf(`expected to be 1`)
+	}
+	if result.MaxZoom != 2 {
+		t.Fatalf(`expected to be 2`)
+	}
+	if result.MinLon != 1.1 {
+		t.Fatalf(`expected to be 1.1`)
+	}
+	if result.MinLat != 2.1 {
+		t.Fatalf(`expected to be 2.1`)
+	}
+	if result.MaxLon != 1.2 {
+		t.Fatalf(`expected to be 1.2`)
+	}
+	if result.MaxLat != 2.2 {
+		t.Fatalf(`expected to be 2.2`)
+	}
+	if result.CenterZoom != 3 {
+		t.Fatalf(`expected to be 3`)
+	}
+	if result.CenterLon != 3.1 {
+		t.Fatalf(`expected to be 3.1`)
+	}
+	if result.CenterLat != 3.2 {
+		t.Fatalf(`expected to be 3.2`)
+	}
+}
