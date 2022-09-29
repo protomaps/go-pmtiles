bc9a7d1429076732ad1b2cc5f04b2adb004a89b7
Brandon Liu <bdon@bdon.org>
2022-09-29 11:39:00 +0800

    refactor mbtiles function into convert.go

--- pmtiles/convert.go
+++ pmtiles/convert.go
@@ -1,11 +1,14 @@
 package pmtiles
 
 import (
+	"encoding/json"
 	"github.com/RoaringBitmap/roaring/roaring64"
 	"github.com/schollz/progressbar/v3"
 	"io/ioutil"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 	"zombiezen.com/go/sqlite"
 )
 
@@ -100,4 +103,93 @@ func Convert(logger *log.Logger, input string, output string) {
 			mbtilesMetadata = append(mbtilesMetadata, stmt.ColumnText(1))
 		}
 	}
+}
+
+func mbtiles_to_header_json(mbtiles_metadata []string) (HeaderV3, map[string]interface{}, error) {
+	header := HeaderV3{}
+	json_result := make(map[string]interface{})
+	for i := 0; i < len(mbtiles_metadata); i += 2 {
+		value := mbtiles_metadata[i+1]
+		switch key := mbtiles_metadata[i]; key {
+		case "format":
+			switch value {
+			case "pbf":
+				header.TileType = Mvt
+			case "png":
+				header.TileType = Png
+			case "jpg":
+				header.TileType = Jpeg
+			case "webp":
+				header.TileType = Webp
+			}
+			json_result["format"] = value
+		case "bounds":
+			parts := strings.Split(value, ",")
+			f, err := strconv.ParseFloat(parts[0], 32)
+			if err != nil {
+				return header, json_result, err
+			}
+			header.MinLon = float32(f)
+			f, err = strconv.ParseFloat(parts[1], 32)
+			if err != nil {
+				return header, json_result, err
+			}
+			header.MinLat = float32(f)
+			f, err = strconv.ParseFloat(parts[2], 32)
+			if err != nil {
+				return header, json_result, err
+			}
+			header.MaxLon = float32(f)
+			f, err = strconv.ParseFloat(parts[3], 32)
+			if err != nil {
+				return header, json_result, err
+			}
+			header.MaxLat = float32(f)
+		case "center":
+			parts := strings.Split(value, ",")
+			f, err := strconv.ParseFloat(parts[0], 32)
+			if err != nil {
+				return header, json_result, err
+			}
+			header.CenterLon = float32(f)
+			f, err = strconv.ParseFloat(parts[1], 32)
+			if err != nil {
+				return header, json_result, err
+			}
+			header.CenterLat = float32(f)
+			i, err := strconv.ParseInt(parts[2], 10, 8)
+			if err != nil {
+				return header, json_result, err
+			}
+			header.CenterZoom = uint8(i)
+		case "minzoom":
+			i, err := strconv.ParseInt(value, 10, 8)
+			if err != nil {
+				return header, json_result, err
+			}
+			header.MinZoom = uint8(i)
+		case "maxzoom":
+			i, err := strconv.ParseInt(value, 10, 8)
+			if err != nil {
+				return header, json_result, err
+			}
+			header.MaxZoom = uint8(i)
+		case "json":
+			var mbtiles_json map[string]interface{}
+			json.Unmarshal([]byte(value), &mbtiles_json)
+			for k, v := range mbtiles_json {
+				json_result[k] = v
+			}
+		case "compression":
+			switch value {
+			case "gzip":
+				header.TileCompression = Gzip
+			}
+			json_result["compression"] = value
+		// name, attribution, description, type, version
+		default:
+			json_result[key] = value
+		}
+	}
+	return header, json_result, nil
 }
