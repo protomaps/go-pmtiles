e4e206fafa0fb5d37bbb01410b1fc3373838940f
Brandon Liu <bdon@bdon.org>
2022-12-28 14:03:58 +0800

    refactor test assertions to be more readable.

--- pmtiles/subpyramid_test.go
+++ pmtiles/subpyramid_test.go
@@ -1,66 +1,31 @@
 package pmtiles
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestPointToTile(t *testing.T) {
-	result := PointToTile(4, 0, 0)
-	if (result != Zxy{4, 8, 8}) {
-		t.Errorf("result did not match, was %d", result)
-	}
-	result = PointToTile(4, -180, -85)
-	if (result != Zxy{4, 0, 15}) {
-		t.Errorf("result did not match, was %d", result)
-	}
-	result = PointToTile(4, -180, 85)
-	if (result != Zxy{4, 0, 0}) {
-		t.Errorf("result did not match, was %d", result)
-	}
-	result = PointToTile(4, 179.999, -85)
-	if (result != Zxy{4, 15, 15}) {
-		t.Errorf("result did not match, was %d", result)
-	}
-	result = PointToTile(4, 179.999, 85)
-	if (result != Zxy{4, 15, 0}) {
-		t.Errorf("result did not match, was %d", result)
-	}
+	assert.Equal(t,Zxy{4, 8, 8},PointToTile(4, 0, 0))
+	assert.Equal(t,Zxy{4, 0, 15},PointToTile(4, -180, -85))
+	assert.Equal(t,Zxy{4, 0, 0},PointToTile(4, -180, 85))
+	assert.Equal(t, Zxy{4, 15, 15},PointToTile(4, 179.999, -85))
+	assert.Equal(t, Zxy{4, 15, 0},PointToTile(4, 179.999, 85))
 }
 
 func TestMatchSameLevel(t *testing.T) {
 	// same level
-	result := Matches(0, 0, 0, 0, 0, Zxy{0, 0, 0})
-	if !result {
-		t.Errorf("result did not match")
-	}
-	result = Matches(4, 0, 0, 8, 8, Zxy{4, 4, 4})
-	if !result {
-		t.Errorf("result did not match")
-	}
+	assert.True(t, Matches(0, 0, 0, 0, 0, Zxy{0, 0, 0}))
+	assert.True(t, Matches(4, 0, 0, 8, 8, Zxy{4, 4, 4}))
 }
 
 func TestMatchCandidateLower(t *testing.T) {
-	result := Matches(4, 0, 0, 8, 8, Zxy{0, 0, 0})
-	if !result {
-		t.Errorf("result did not match")
-	}
-	result = Matches(4, 15, 15, 15, 15, Zxy{0, 0, 0})
-	if !result {
-		t.Errorf("result did not match")
-	}
-	result = Matches(4, 15, 15, 15, 15, Zxy{1, 0, 0})
-	if result {
-		t.Errorf("result did not match")
-	}
-	result = Matches(4, 15, 15, 15, 15, Zxy{1, 1, 1})
-	if !result {
-		t.Errorf("result did not match")
-	}
+	assert.True(t,Matches(4, 0, 0, 8, 8, Zxy{0, 0, 0}))
+	assert.True(t,Matches(4, 15, 15, 15, 15, Zxy{0, 0, 0}))
+	assert.False(t,Matches(4, 15, 15, 15, 15, Zxy{1, 0, 0}))
+	assert.True(t,Matches(4, 15, 15, 15, 15, Zxy{1, 1, 1}))
 }
 
 func TestMatchCandidateHigher(t *testing.T) {
-	result := Matches(4, 0, 0, 8, 8, Zxy{8, 0, 0})
-	if !result {
-		t.Errorf("result did not match")
-	}
+	assert.True(t,Matches(4, 0, 0, 8, 8, Zxy{8, 0, 0}))
 }
--- pmtiles/tile_id_test.go
+++ pmtiles/tile_id_test.go
@@ -6,45 +6,27 @@ import (
 )
 
 func TestZxyToId(t *testing.T) {
-	id := ZxyToId(0, 0, 0)
-	if id != 0 {
-		t.Fatalf(`expected %d to be 0`, id)
-	}
-	id = ZxyToId(1, 0, 0)
-	if id != 1 {
-		t.Fatalf(`expected %d to be 1`, id)
-	}
-	id = ZxyToId(1, 0, 1)
-	if id != 2 {
-		t.Fatalf(`expected %d to be 2`, id)
-	}
-	id = ZxyToId(1, 1, 1)
-	if id != 3 {
-		t.Fatalf(`expected %d to be 3`, id)
-	}
-	id = ZxyToId(1, 1, 0)
-	if id != 4 {
-		t.Fatalf(`expected %d to be 4`, id)
-	}
-	id = ZxyToId(2, 0, 0)
-	if id != 5 {
-		t.Fatalf(`expected %d to be 5`, id)
-	}
+	assert.Equal(t, uint64(0), ZxyToId(0, 0, 0))
+	assert.Equal(t, uint64(1), ZxyToId(1, 0, 0))
+	assert.Equal(t, uint64(2), ZxyToId(1, 0, 1))
+	assert.Equal(t, uint64(3), ZxyToId(1, 1, 1))
+	assert.Equal(t, uint64(4), ZxyToId(1, 1, 0))
+	assert.Equal(t, uint64(5), ZxyToId(2, 0, 0))
 }
 
 func TestIdToZxy(t *testing.T) {
 	z, x, y := IdToZxy(0)
-	if !(z == 0 && x == 0 && y == 0) {
-		t.Fatalf(`expected to be (0,0,0)`)
-	}
+	assert.Equal(t, uint8(0), z)
+	assert.Equal(t, uint32(0), x)
+	assert.Equal(t, uint32(0), y)
 	z, x, y = IdToZxy(1)
-	if !(z == 1 && x == 0 && y == 0) {
-		t.Fatalf(`expected to be (1,0,0)`)
-	}
+	assert.Equal(t, uint8(1), z)
+	assert.Equal(t, uint32(0), x)
+	assert.Equal(t, uint32(0), y)
 	z, x, y = IdToZxy(19078479)
-	if !(z == 12 && x == 3423 && y == 1763) {
-		t.Fatalf(`expected to be (12,3423,1763)`)
-	}
+	assert.Equal(t, uint8(12), z)
+	assert.Equal(t, uint32(3423), x)
+	assert.Equal(t, uint32(1763), y)
 }
 
 func TestManyTileIds(t *testing.T) {
