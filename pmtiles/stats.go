5ff8d40b98ddcf5b905cb75e90e131c42f0ac189
Brandon Liu <bdon@bdon.org>
2023-09-21 21:44:04 +0800

    wip on stats

--- pmtiles/stats.go
+++ pmtiles/stats.go
@@ -5,7 +5,6 @@ import (
 	"bytes"
 	"compress/gzip"
 	"context"
-	"encoding/csv"
 	"fmt"
 	"github.com/paulmach/protoscan"
 	"github.com/schollz/progressbar/v3"
@@ -13,22 +12,11 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"math"
+	"golang.org/x/sync/errgroup"
 	"runtime"
-	"strconv"
 	"time"
 )
-
-type Task struct {
-	Index  int
-	TileId uint64
-	Data   []byte
-	Result chan TaskResult
-}
-
-type TaskResult struct {
-	Index int
-	Rows  [][]string
-}
 
 // layer name, # features, attr bytes, # attr values
 func Layer(msg *protoscan.Message) (string, int, int, int) {
@@ -63,51 +51,58 @@ func Layer(msg *protoscan.Message) (string, int, int, int) {
 	return name, features, attr_bytes, values
 }
 
-func calcStats(tileid uint64, tilebytes []byte) [][]string {
-	z, x, y := IdToZxy(tileid)
-	gzreader, err := gzip.NewReader(bytes.NewReader(tilebytes))
-	if err != nil {
-		panic(err)
-	}
-	decoded, err := ioutil.ReadAll(gzreader)
-	if err != nil {
-		panic(err)
-	}
-	msg := protoscan.New(decoded)
-	var m *protoscan.Message
+func calcStats(parts []uint64, data []byte) []byte {
+	var buf bytes.Buffer
+	reader := bytes.NewReader(data)
 
-	rows := make([][]string, 0)
-	for msg.Next() {
-		switch msg.FieldNumber() {
-		case 3:
-			m, err = msg.Message(m)
-			if err != nil {
-				panic(err)
+	for i := 0; i < len(parts); i += 2 {
+		tile_id := parts[i]
+		z, x, y := IdToZxy(tile_id)
+
+		limit := &io.LimitedReader{reader, int64(parts[i+1])}
+		gz_reader, err := gzip.NewReader(limit)
+		if err != nil {
+			panic(err)
+		}
+		decoded, err := ioutil.ReadAll(gz_reader)
+		if err != nil {
+			panic(err)
+		}
+		msg := protoscan.New(decoded)
+		var m *protoscan.Message
+
+		for msg.Next() {
+			switch msg.FieldNumber() {
+			case 3:
+				m, err = msg.Message(m)
+				if err != nil {
+					panic(err)
+				}
+				name, features, attr_bytes, attr_values := Layer(m)
+				buf.WriteString(fmt.Sprintf("%d\t%d\t%d\t%s\t%d\t%d\t%d\n",z,x,y,name,features,attr_bytes,attr_values))
+			default:
+				msg.Skip()
 			}
-			name, features, attr_bytes, attr_values := Layer(m)
-			rows = append(rows, []string{
-				strconv.FormatUint(tileid, 10),
-				strconv.FormatUint(uint64(z), 10),
-				strconv.FormatUint(uint64(x), 10),
-				strconv.FormatUint(uint64(y), 10),
-				strconv.FormatUint(uint64(len(tilebytes)), 10),
-				name,
-				strconv.Itoa(len(m.Data)),
-				strconv.Itoa(features),
-				strconv.Itoa(attr_bytes),
-				strconv.Itoa(attr_values),
-			})
+		}
 
-		default:
-			msg.Skip()
+		if msg.Err() != nil {
+			panic(msg.Err())
 		}
 	}
 
-	if msg.Err() != nil {
-		panic(msg.Err())
-	}
+	return buf.Bytes()
+}
 
-	return rows
+type Task struct {
+	Index  int
+	Parts []uint64 // N pairs of TileID, Length
+	Data  []byte // N compressed tiles
+	Result chan TaskResult
+}
+
+type TaskResult struct {
+	Index int
+	Rows  []byte
 }
 
 func Stats(logger *log.Logger, file string) error {
@@ -167,49 +162,61 @@ func Stats(logger *log.Logger, file string) error {
 		}
 	}
 
+	// TODO: pre-determine all duplicate tiles.
+
 	output, err := os.Create(file + ".layerstats.tsv.gz")
 	if err != nil {
-		return fmt.Errorf("Failed to create output")
+		panic(err)
 	}
 	defer output.Close()
 
 	bar := progressbar.Default(
-		int64(header.TileEntriesCount),
+		int64(header.TileContentsCount),
 		"writing stats",
 	)
 
 	f, err := os.Open(file)
-	defer f.Close()
-	f.Seek(int64(header.TileDataOffset), io.SeekStart)
-	buffered := bufio.NewReaderSize(f, 10000000)
+	if err != nil {
+		panic(err)
+	}
 
-	tasks := make(chan Task, 100000)
-	intermediate := make(chan TaskResult, 100000)
+	defer f.Close()
+
+	IO_BYTES := 10000000
+	CHANNEL_SIZE := 10000
+	TASK_TILE_CONTENTS := 1000
+
+	f.Seek(int64(header.TileDataOffset), io.SeekStart)
+	buffered := bufio.NewReaderSize(f, IO_BYTES)
+
+	tasks := make(chan Task, CHANNEL_SIZE)
+	intermediate := make(chan TaskResult, CHANNEL_SIZE)
+
+	errs, _ := errgroup.WithContext(ctx)
 
 	// workers
 	for i := 0; i < runtime.GOMAXPROCS(0); i++ {
-		go func() {
+		errs.Go(func() error {
 			for task := range tasks {
-				rows := calcStats(task.TileId, task.Data)
+				rows := calcStats(task.Parts, task.Data)
 				task.Result <- TaskResult{task.Index, rows}
+				bar.Add(len(task.Parts) / 2)
 			}
-		}()
+			return nil
+		})
 	}
 
 	// collector
 	// buffers the results, outputting them in exact sorted order
 	// once it has received all results, it terminates
-	lastTask := int(header.TileContentsCount) - 1
+	endTask := int(math.Ceil(float64(header.TileContentsCount) / float64(TASK_TILE_CONTENTS))) + 1
+	fmt.Println(endTask)
+
 	go func() {
 		gzWriter, _ := gzip.NewWriterLevel(output,gzip.BestSpeed)
 		defer gzWriter.Close()
 
-		csvWriter := csv.NewWriter(gzWriter)
-		csvWriter.Comma = '\t'
-		defer csvWriter.Flush()
-		if err := csvWriter.Write([]string{"hilbert", "z", "x", "y", "archive_tile_bytes", "layer", "layer_bytes", "layer_features", "attr_bytes", "attr_values"}); err != nil {
-			panic(fmt.Errorf("Failed to write header to TSV: %v", err))
-		}
+		gzWriter.Write([]byte("hilbert,z,x,y,archive_tile_bytes,layer,layer_bytes,layer_features,attr_bytes,attr_values\n"))
 
 		buffer := make(map[int]TaskResult)
 		nextIndex := 0
@@ -219,16 +226,12 @@ func Stats(logger *log.Logger, file string) error {
 
 			for {
 				if next, ok := buffer[nextIndex]; ok {
-					for _, row := range next.Rows {
-						if err := csvWriter.Write(row); err != nil {
-							panic(fmt.Errorf("Failed to write record to TSV: %v", err))
-						}
-					}
+					gzWriter.Write(next.Rows)
 
 					delete(buffer, nextIndex)
 					nextIndex++
 
-					if nextIndex == lastTask {
+					if nextIndex == endTask {
 						close(intermediate)
 					}
 				} else {
@@ -238,21 +241,49 @@ func Stats(logger *log.Logger, file string) error {
 		}
 	}()
 
-	idx := 0
+	task_index := 0
 	current_offset := uint64(0)
+	parts := make([]uint64, 0)
+	block_len := uint64(0)
+
 	CollectEntries(header.RootOffset, header.RootLength, func(e EntryV3) {
-		bar.Add(1)
+		// we only count unique tile entries
 		if e.Offset == current_offset {
-			tilebytes := make([]byte, int64(e.Length))
+			parts = append(parts, e.TileId)
+			parts = append(parts, uint64(e.Length))
+			block_len += uint64(e.Length)
+
+			if len(parts) == TASK_TILE_CONTENTS*2 {
+				tilebytes := make([]byte, int64(block_len))
+				_, err := io.ReadFull(buffered, tilebytes)
+				if err != nil {
+					panic(err)
+				}
+				tasks <- Task{Index: task_index, Parts:parts, Data: tilebytes, Result: intermediate}
+				task_index += 1
+				parts = nil
+				block_len = 0
+			}
+
+			current_offset += uint64(e.Length)
+		}
+	})
+
+	// flush the last part
+	if block_len > 0  {
+			tilebytes := make([]byte, int64(block_len))
 			_, err := io.ReadFull(buffered, tilebytes)
 			if err != nil {
 				panic(err)
 			}
-			tasks <- Task{Index: idx, TileId: e.TileId, Data: tilebytes, Result: intermediate}
-			idx += 1
-			current_offset += uint64(e.Length)
-		}
-	})
+			tasks <- Task{Index: task_index, Parts:parts, Data: tilebytes, Result: intermediate}
+	}
+	close(tasks)
+
+	err = errs.Wait()
+	if err != nil {
+		return err
+	}
 
 	fmt.Printf("Completed stats in %v.\n", time.Since(start))
 	return nil
