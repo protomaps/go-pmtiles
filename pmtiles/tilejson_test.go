39e5b13bb103df1da8de0a5f4b3c1256819eed48
Mike O'Toole <mike.otoole@gmail.com>
2023-12-24 16:41:52 +0000

    starting tests

--- pmtiles/tilejson_test.go
+++ pmtiles/tilejson_test.go
@@ -1,1 +1,50 @@
-git 
+package pmtiles
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateTilejson(t *testing.T) {
+	// Define test inputs
+	header := HeaderV3{
+		// Define header fields
+	}
+	metadataBytes := []byte(`
+	{
+		"vector_layers": [{"id": "layer1"}],
+		"attribution": "Attribution",
+		"description": "Description",
+		"name": "Name",
+		"version": "1.0"
+	}`)
+	tileURL := "https://example.com/tiles/{z}/{x}/{y}.pbf"
+
+	// Call the function
+	tilejsonBytes, err := CreateTilejson(header, metadataBytes, tileURL)
+
+	// Check for errors
+	if err != nil {
+		t.Errorf("CreateTilejson returned an error: %v", err)
+	}
+
+	// Parse the tilejsonBytes to check the output
+	var tilejson map[string]interface{}
+	err = json.Unmarshal(tilejsonBytes, &tilejson)
+	if err != nil {
+		t.Errorf("Failed to parse the generated TileJSON: %v", err)
+	}
+
+	assert.Equal(t, "3.0.0", tilejson["tilejson"])
+	assert.Equal(t, "xyz", tilejson["scheme"])
+
+	// Add assertions to validate the generated TileJSON
+	// For example:
+	// if tilejson.Name != "My Tileset" {
+	// 	t.Errorf("Unexpected tileset name. Expected: %s, Got: %s", "My Tileset", tilejson.Name)
+	// }
+
+	// Add more test cases as needed
+}
