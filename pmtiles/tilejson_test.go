2fa5a2762a2b4c37ffd4b53a0dccd7b59633c17b
Brandon Liu <bdon@bdon.org>
2024-01-09 15:20:34 +0800

    TileJSON does not emit null values for optional string fields [#110]

--- pmtiles/tilejson_test.go
+++ pmtiles/tilejson_test.go
@@ -18,6 +18,7 @@ func TestCreateTilejson(t *testing.T) {
 		MaxLatE7:    483000000,
 		CenterLonE7: -1141500000,
 		CenterLatE7: 481000000,
+		TileType:    Mvt,
 	}
 	metadataBytes := []byte(`
 	{
@@ -27,7 +28,7 @@ func TestCreateTilejson(t *testing.T) {
 		"name": "Name",
 		"version": "1.0"
 	}`)
-	tileURL := "https://example.com/tiles.pmtiles/{z}/{x}/{y}"
+	tileURL := "https://example.com/foo"
 
 	// Call the function
 	tilejsonBytes, err := CreateTilejson(header, metadataBytes, tileURL)
@@ -46,7 +47,7 @@ func TestCreateTilejson(t *testing.T) {
 
 	assert.Equal(t, "3.0.0", tilejson["tilejson"])
 	assert.Equal(t, "xyz", tilejson["scheme"])
-	assert.Equal(t, []interface{}{"https://example.com/tiles.pmtiles/{z}/{x}/{y}"}, tilejson["tiles"])
+	assert.Equal(t, []interface{}{"https://example.com/foo/{z}/{x}/{y}.mvt"}, tilejson["tiles"])
 	assert.Equal(t, []interface{}{map[string]interface{}{"id": "layer1"}}, tilejson["vector_layers"])
 	assert.Equal(t, "Attribution", tilejson["attribution"])
 	assert.Equal(t, "Description", tilejson["description"])
@@ -57,4 +58,42 @@ func TestCreateTilejson(t *testing.T) {
 	assert.Equal(t, []interface{}{-114.150000, 48.100000, 0.0}, tilejson["center"])
 	assert.Equal(t, 0.0, tilejson["minzoom"])
 	assert.Equal(t, 14.0, tilejson["maxzoom"])
+}
+
+func TestCreateTilejsonOptionalFields(t *testing.T) {
+	header := HeaderV3{
+		MinZoom:     0.0,
+		MaxZoom:     14.0,
+		MinLonE7:    -1144000000,
+		MinLatE7:    479000000,
+		MaxLonE7:    -1139000000,
+		MaxLatE7:    483000000,
+		CenterLonE7: -1141500000,
+		CenterLatE7: 481000000,
+		TileType:    Png,
+	}
+	metadataBytes := []byte(`
+	{
+	}`)
+
+	tilejsonBytes, err := CreateTilejson(header, metadataBytes, "")
+
+	// Check for errors
+	if err != nil {
+		t.Errorf("CreateTilejson returned an error: %v", err)
+	}
+
+	var tilejson map[string]interface{}
+	err = json.Unmarshal(tilejsonBytes, &tilejson)
+	if err != nil {
+		t.Errorf("Failed to parse the generated TileJSON: %v", err)
+	}
+
+	assert.Equal(t, "3.0.0", tilejson["tilejson"])
+	assert.Equal(t, "xyz", tilejson["scheme"])
+	assert.Equal(t, []interface{}{"https://example.com/{z}/{x}/{y}.png"}, tilejson["tiles"])
+	assert.NotContains(t, tilejson, "attribution")
+	assert.NotContains(t, tilejson, "description")
+	assert.NotContains(t, tilejson, "name")
+	assert.NotContains(t, tilejson, "version")
 }
