faa83c707a1f79dab2612c5c56ff0617073f7b91
Brandon Liu <bdon@bdon.org>
2024-09-15 15:51:00 +0800

    fmt

--- pmtiles/upload_test.go
+++ pmtiles/upload_test.go
@@ -1,12 +1,11 @@
 package pmtiles
 
 import (
-	"testing"
 	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
-
 func TestPartSizeBytes(t *testing.T) {
-	assert.Equal(t, 5 * 1024 * 1024, partSizeBytes(100))
-	assert.Equal(t, 6442451, partSizeBytes(60 * 1024 * 1024 * 1024))
+	assert.Equal(t, 5*1024*1024, partSizeBytes(100))
+	assert.Equal(t, 6442451, partSizeBytes(60*1024*1024*1024))
 }
