274916711791688b38f275a88f7f58a8c85d6c7e
zstadler <zeev.stadler@gmail.com>
2024-12-01 13:47:38 +0200

    Update `pmtiles upload --help`
    Related to #194

--- main.go
+++ main.go
@@ -109,8 +109,8 @@ var cli struct {
 	} `cmd:"" help:"Download a local archive to remote storage." hidden:""`
 
 	Upload struct {
-		Input          string `arg:"" type:"existingfile"`
-		Key            string `arg:""`
+		InputPMTiles   string `arg:"" type:"existingfile" help:"The local PMTiles file"`
+		RemotePMTiles  string `arg:""  help:"The name for the remote PMTiles source"`
 		MaxConcurrency int    `default:"2" help:"# of upload threads"`
 		Bucket         string `required:"" help:"Bucket to upload to."`
 	} `cmd:"" help:"Upload a local archive to remote storage."`
@@ -203,8 +203,8 @@ func main() {
 		if err != nil {
 			logger.Fatalf("Failed to convert %s, %v", path, err)
 		}
-	case "upload <input> <key>":
-		err := pmtiles.Upload(logger, cli.Upload.Input, cli.Upload.Bucket, cli.Upload.Key, cli.Upload.MaxConcurrency)
+	case "upload <inputpmiles> <remotepmtiles>":
+		err := pmtiles.Upload(logger, cli.Upload.InputPMTiles, cli.Upload.Bucket, cli.Upload.RemotePMTiles, cli.Upload.MaxConcurrency)
 
 		if err != nil {
 			logger.Fatalf("Failed to upload file, %v", err)
